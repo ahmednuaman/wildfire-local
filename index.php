e983be0d725ea171d6dc30be7c1d9b596556572b
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-09-25 16:30:24 +0200

    Update index.php
    added additional tags

--- index.php
+++ index.php
@@ -45,8 +45,11 @@ class Bootstrap
         $renderer = new LiquidTemplate($this->path);
 
         // register shiz
+        $renderer->registerTag('formblock', 'LiquidTagFormBlock');
         $renderer->registerTag('marker', 'LiquidTagMarker');
         $renderer->registerTag('plugin', 'LiquidTagPlugin');
+        $renderer->registerTag('text', 'LiquidTagText');
+        $renderer->registerTag('unless', 'LiquidTagUnless');
         $renderer->registerFilter(new LiquidFilterAssetsURL());
 
         // parse the template
@@ -101,24 +104,160 @@ class Bootstrap
     }
 }
 
-class LiquidTagMarker extends LiquidBlock
+class LiquidBlockExtended extends LiquidBlock
 {
-    public function render(&$context)
+    public function unknownTag($tag, $params, &$tokens)
     {
-        return parent::render($context);
+        echo $tag;
     }
 }
 
-class LiquidTagPlugin extends LiquidBlock
+class LiquidTagFormBlock extends LiquidBlockExtended {}
+class LiquidTagMarker extends LiquidBlockExtended {}
+
+class LiquidTagPlugin extends LiquidBlockExtended 
 {
     public function __construct($markup, &$tokens, &$fileSystem)
     {
-        // nothing
+        $this->_markup = str_replace(' ', '', $markup);
     }
 
     public function render(&$context)
     {
         return '';
+    }
+}
+
+class LiquidTagText extends LiquidTagPlugin
+{
+    public function render(&$context)
+    {
+        return $context->get($this->_markup);
+    }
+}
+
+class LiquidTagUnless extends LiquidDecisionBlock
+{
+    private $_nodelistHolders = array();
+    private $_blocks = array();
+
+    public function __construct($markup, &$tokens, &$fileSystem)
+    {
+        $this->_nodelist = &$this->_nodelistHolders[count($this->_blocks)];
+
+        array_push($this->_blocks, array(
+            'unless', $markup, &$this->_nodelist
+        ));
+
+        parent::__construct($markup, $tokens, $fileSystem);
+    }
+
+    public function unknownTag($tag, $params, &$tokens)
+    {
+        if ($tag == 'else')
+        {
+            /* Update reference to nodelistHolder for this block */
+            $this->_nodelist = &$this->_nodelistHolders[count($this->_blocks) + 1];
+            $this->_nodelistHolders[count($this->_blocks) + 1] = array();
+
+            array_push($this->_blocks, array(
+                $tag, $params, &$this->_nodelist
+            ));
+
+        }
+        else
+        {
+            parent::unknownTag($tag, $params, $tokens);
+        }
+    }
+
+    public function render(&$context)
+    {
+        $context->push();
+
+        $logicalRegex = new LiquidRegexp('/\s+(and|or)\s+/');
+        $conditionalRegex = new LiquidRegexp('/(' . LIQUID_QUOTED_FRAGMENT . ')\s*([=!<>a-z_]+)?\s*(' . LIQUID_QUOTED_FRAGMENT . ')?/');
+
+        $result = '';
+
+        foreach($this->_blocks as $i => $block)
+        {
+            if ($block[0] == 'else')
+            {
+                $result = $this->renderAll($block[2], $context);
+
+                break;
+            }
+
+            if ($block[0] == 'unless')
+            {
+                /* Extract logical operators */
+                $logicalRegex->match($block[1]);
+
+                $logicalOperators = $logicalRegex->matches;
+                array_shift($logicalOperators);
+
+                /* Extract individual conditions */
+                $temp = $logicalRegex->split($block[1]);
+
+                $conditions = array();
+
+                foreach($temp as $condition)
+                {
+                    if ($conditionalRegex->match($condition))
+                    {
+                        $left = (isset($conditionalRegex->matches[1])) ? $conditionalRegex->matches[1] : null;
+                        $operator = (isset($conditionalRegex->matches[2])) ? $conditionalRegex->matches[2] : null;
+                        $right = (isset($conditionalRegex->matches[3])) ? $conditionalRegex->matches[3] : null;
+
+                        array_push($conditions, array(
+                                'left' => $left,
+                                'operator' => $operator,
+                                'right' => $right
+                        ));
+                    }
+                    else
+                    {
+                        throw new LiquidException("Syntax Error in tag 'if' - Valid syntax: if [condition]");
+                    }
+                }
+
+                if (count($logicalOperators))
+                {
+                    /* If statement contains and/or */
+                    $display = true;
+
+                    foreach($logicalOperators as $k => $logicalOperator)
+                    {
+                        if ($logicalOperator == 'and')
+                        {
+                            $display = !$this->_interpretCondition($conditions[$k]['left'], $conditions[$k]['right'], $conditions[$k]['operator'], $context) && $this->_interpretCondition($conditions[$k + 1]['left'], $conditions[$k + 1]['right'], $conditions[$k + 1]['operator'], $context);
+                        }
+                        else
+                        {
+                            $display = !$this->_interpretCondition($conditions[$k]['left'], $conditions[$k]['right'], $conditions[$k]['operator'], $context) || $this->_interpretCondition($conditions[$k + 1]['left'], $conditions[$k + 1]['right'], $conditions[$k + 1]['operator'], $context);
+                        }
+                    }
+
+                }
+                else
+                {
+                    /* If statement is a single condition */
+                    $display = !$this->_interpretCondition($conditions[0]['left'], $conditions[0]['right'], $conditions[0]['operator'], $context);
+                }
+
+                if ($display)
+                {
+                    $result = $this->renderAll($block[2], $context);
+
+                    break;
+                }
+            }
+        }
+
+        $context->pop();
+
+        return $result;
     }
 }
 
