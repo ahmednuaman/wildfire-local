044dba610c6f09139358575d114d04810b765ce1
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-09-26 12:20:53 +0100

    included jquery in base

--- base.php
+++ base.php
@@ -5,6 +5,7 @@
     {% for stylesheet in stylesheets %}
         <link rel="stylesheet" type="text/css" href="{{ 'assets/' | asset_url }}{{ stylesheet }}.css" />
     {% endfor %}
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
     <title></title>
 </head>
 <body>
